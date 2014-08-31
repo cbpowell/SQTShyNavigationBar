cd98ec9de759971a016c49f39feefac0ed8b8926
Charles Powell <cbpowell@gmail.com>
2014-08-30 19:49:03 -0700

    Initial Commit

---
+++ SQTShyNavigationBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SQTShyNavigationBar
+//
+//  Created by Charles Powell on 8/30/14.
+//  Copyright (c) 2014 Charles Powell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
